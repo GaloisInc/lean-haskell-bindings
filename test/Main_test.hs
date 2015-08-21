ad7ff86d50c4a7a362831fe24f869976ecb34acf
Joe Hendrix <jhendrix@galois.com>
2015-08-21 14:23:10 -0700

    Start using test framework for tests.

--- test/Main_test.hs
+++ test/Main_test.hs
@@ -1,44 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import qualified Language.Lean as Lean
-import Data.Monoid
-import Control.Exception
+import Test.Tasty
+import Test.Tasty.Ingredients
+import Test.Tasty.Runners.AntXML
+
+import Tests.Name (nameTests)
 
 main :: IO ()
-main = do
-  putStrLn "Started name test"
-  let a = Lean.anonymousName
-  case Lean.viewName a of
-    Lean.AnonymousName -> return ()
-    _ -> fail "Expected anonymous name."
+main = defaultMainWithIngredients ingrs tests
 
-  let n2 = "foo.bla"
+ingrs :: [Ingredient]
+ingrs = antXMLRunner : defaultIngredients
 
-  putStrLn $ "Lean name: " ++ show n2
-
-  case Lean.viewName n2 of
-    Lean.StringName{} -> return ()
-    _ -> fail "Expected string name"
-
-  let n3 = n2 `Lean.idxName` 1
-  putStrLn $ "Lean name: " ++ show n3
-
-  case Lean.viewName n3 of
-    Lean.IndexName n4 1 | n4 == n2 -> do
-      return ()
-    _ -> fail $ "Expected index name"
-
-  -- TODO: Test exception generation on partial function
-
-{-
-    check(!lean_get_name_prefix(a, &n5, &ex));
-    s3 = lean_get_exception_message(ex);
-    printf("Lean exception: %s\n", s3);
--}
-
-  let badName = "foo.1dog"
-  res <- try $ (return $! badName) :: IO (Either Lean.LeanException Lean.Name)
-  case res of
-    Left _ -> return ()
-    Right _ -> fail "Expected failure while building Lean name."
+tests :: TestTree
+tests = testGroup "lean-bindings"
+  [ nameTests
+  ]
---
+++ test/Tests/Name.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Tests.Name (nameTests) where
+
+import Control.Exception (try)
+import Control.Monad
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import qualified Language.Lean as Lean
+
+nameTests :: TestTree
+nameTests = testGroup "Name"
+  [ testCase "anonymous" anonymousTest
+  , testCase "string" stringTest
+  , testCase "idx" idxTest
+  , testCase "bad_name" badNameTest
+  ]
+
+anonymousTest :: IO ()
+anonymousTest = do
+  case Lean.viewName Lean.anonymousName of
+    Lean.AnonymousName -> return ()
+    _ -> fail "Expected anonymous name."
+
+stringTest :: IO ()
+stringTest = do
+  case Lean.viewName "foo.bla" of
+    Lean.StringName "foo" "bla" -> return ()
+    _ -> fail "Expected string name"
+
+  when (show ("foo.bla" :: Lean.Name) /= "foo.bla") $ do
+    fail "Name did not match expected."
+
+idxTest :: IO ()
+idxTest = do
+  case Lean.viewName "foo.bla.1" of
+    Lean.IndexName "foo.bla" 1 -> do
+      return ()
+    _ -> fail $ "Expected index name"
+
+badNameTest :: IO ()
+badNameTest = do
+  res <- try $ (return $! "foo.1dog") :: IO (Either Lean.LeanException Lean.Name)
+  case res of
+    Left _ -> return ()
+    Right _ -> fail "Expected failure while building Lean name."
