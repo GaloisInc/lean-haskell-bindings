b3491d5c194681f580b7d832924644437f5beb5a
Joe Hendrix <jhendrix@galois.com>
2015-08-25 14:40:42 -0700

    Fix build errors.

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-echo "$PATH"
-
 export PATH="$PWD/.cabal-sandbox/bin:$PATH"
 export PATH="/opt/ghc/7.10.2/bin:$PATH"
-
-which ghc
-which cabl
 
 .cabal-sandbox/bin/cabal configure --enable-tests
 .cabal-sandbox/bin/cabal build
