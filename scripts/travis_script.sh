ea361fedc21da1edbf12bc757a2c72084efb1fa8
Joe Hendrix <jhendrix@galois.com>
2015-09-02 11:06:03 -0700

    Fix cabal build commands

--- scripts/travis_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH="$PWD/.cabal-sandbox/bin:$PATH"
-export PATH="/opt/ghc/7.10.2/bin:$PATH"
-
-.cabal-sandbox/bin/cabal configure --enable-tests
-.cabal-sandbox/bin/cabal build
-LD_LIBRARY_PATH="$PWD/deps/lean/build" LEAN_PATH="$PWD/deps/lean/library" .cabal-sandbox/bin/cabal test
