de5fcc8a57f45bc722d8a5e126558f7f6a1090ad
Joe Hendrix <jhendrix@galois.com>
2015-09-02 10:01:22 -0700

    Update build to use travis cache

--- scripts/get_dependencies.sh
+++ scripts/get_dependencies.sh
@@ -4,65 +4,45 @@ set -ex
 ########################################################################
 ## Cabal packages
 
-if [ ! -f cabal.config ]; then
-    wget https://www.stackage.org/lts/cabal.config -O - > cabal.config
+wget https://www.stackage.org/lts/cabal.config -O - > cabal.config
+
+if diff -u cabal.config .cabsnap/cabal.config; then
+    echo "Stackage config cache hit"
+    rm -rfv $HOME/.ghc;
+    cp -a $HOME/.cabsnap/ghc $HOME/.ghc;
+    cp -a $HOME/.cabsnap/lib $HOME/.cabsnap/share $HOME/.cabsnap/bin $HOME/.cabsnap/config $HOME/.cabal/;
+else
+    echo "Stackage config cache miss"
+    rm -rf $HOME/.cabsnap;
+    travis_retry cabal update -v
+    sed -i 's/^jobs:/-- jobs:/' $HOME/.cabal/config
+    cabal install c2hs
+    cabal install --only-dependencies --enable-tests
+    echo "snapshotting configuration to build-cache";
+    mkdir $HOME/.cabsnap;
+    cp -a cabal.config $HOME/.cabsnap/config
+    cp -a $HOME/.ghc $HOME/.cabsnap/ghc;
+    cp -a $HOME/.cabal/lib $HOME/.cabal/share $HOME/.cabal/bin $HOME/.cabal/config $HOME/.cabsnap/;
 fi
-
-ls -l /opt/ghc/7.10.2
-
-export PATH="$PWD/.cabal-sandbox/bin:$PATH"
-export PATH="/opt/ghc/7.10.2/bin:$PATH"
-which ghc
-
-cabal sandbox init
-cabal update
-
-ls -la .
-ls -l .cabal-sandbox || echo "sandbox not found"
-ls -l .cabal-sandbox/bin || echo "not found"
-
-type cabal
-type alex
-
-cabal install -v cabal-install
-CABAL=$(which cabal)
-
-type cabal
-cabal --version
-$CABAL --version
-
-$CABAL install alex
-$CABAL install happy
-
-cabal install c2hs
-cabal install --only-dependencies --enable-tests
 
 ########################################################################
 ## Lean
 
-mkdir -p deps
-
-pushd deps > /dev/null
-
-if [ -d lean ]; then
-    pushd lean > /dev/null
+if [ -d deps/lean ]; then
+    pushd deps/lean > /dev/null
     git pull
+    popd > /dev/null # deps/lean
 else
-    git clone https://github.com/leanprover/lean.git
-    pushd lean > /dev/null
+    git clone https://github.com/leanprover/lean.git deps/lean
 fi
 
-mkdir -p build
-pushd build > /dev/null
-
+mkdir -p deps/lean/build
+pushd deps/lean/build > /dev/null
 if [ ! -f Makefile ]; then
-    cmake -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_CXX_COMPILER=g++-4.9 ../src
+    cmake -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_CXX_COMPILER=$CXX ../src
 fi
-
-make || cat Makefile
+make
 popd > /dev/null # deps/lean/build
-popd > /dev/null # deps/lean
-popd > /dev/null # deps
 
 ########################################################################
 ## cabal.config
